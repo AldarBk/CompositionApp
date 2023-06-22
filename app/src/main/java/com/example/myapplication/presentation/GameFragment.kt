18b03132ac6c8bb184939ff276b1c23a2008235d
AldarBk <aldarrazer20@yandex.ru>
2023-06-22 11:56:17 +0600

    Fragments navigAdd

--- app/src/main/java/com/example/myapplication/presentation/GameFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/GameFragment.kt
@@ -7,25 +7,72 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentGameBinding
-import com.example.myapplication.databinding.FragmentGameFinishedBinding
+import com.example.myapplication.domain.entity.GameResult
+import com.example.myapplication.domain.entity.GameSettings
 
 class GameFragment : Fragment() {
 
+    private lateinit var level: com.example.myapplication.domain.entity.Level
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvOption1.setOnClickListener {
+            launchGameFinishedFragment(
+                GameResult(
+                    true,
+                    0,
+                    0,
+                    GameSettings(0,0,0,0)
+                )
+            )
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as com.example.myapplication.domain.entity.Level
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+
+        private const val KEY_LEVEL = "level"
+
+
+        fun newInstance(level: com.example.myapplication.domain.entity.Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
